756928ee4e19a7153b6887941550807b652cd735
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-11-24 20:19:32 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 구현

--- src/Computer.js
+++ src/Computer.js
@@ -1,3 +1,18 @@
-class Computer {}
+const { Random } = require('@woowacourse/mission-utils');
+
+class Computer {
+  #randomNumbers;
+
+  initRandomNumbers() {
+    const numbers = new Set();
+
+    while (numbers.size !== 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      numbers.add(randomNumber);
+    }
+
+    this.#randomNumbers = [...numbers];
+  }
+}
 
 module.exports = Computer;
