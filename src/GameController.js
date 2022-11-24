daa41209c03c3af7345ae9bd7945db5c60515e3a
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-11-24 20:38:05 +0900

    feat: 사용자 숫자 입력 요청 기능 구현
    Inputview: 사용자 input요청 메서드를 모아둔 객체

--- src/GameController.js
+++ src/GameController.js
@@ -1,5 +1,6 @@
 const BaseballGame = require('./BaseballGame.js');
 const Computer = require('./Computer.js');
+const InputView = require('./InputView.js');
 
 class GameController {
   #baseballGame;
@@ -7,6 +8,12 @@ class GameController {
   start() {
     const computer = new Computer();
     this.#baseballGame = new BaseballGame(computer);
+
+    this.#requestUserNumbers();
+  }
+
+  #requestUserNumbers() {
+    InputView.requestUserNumbers((userNumbers) => {});
   }
 }
 
