2b007944a2740138a5e9f0f7b7496a2c5714263e
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-11-25 13:41:28 +0900

    refactor: 에러시 throw후 게임 종료 하도록 개선

--- src/Validation.js
+++ src/Validation.js
@@ -1,6 +1,8 @@
 class Validation {
   static validateUserNumbers(input) {
-    return this.#hasOnlyNumber(input);
+    if (!this.#hasOnlyNumber(input)) {
+      throw new Error('[ERROR] 숫자가 아닌 문자가 포함되었습니다.');
+    }
   }
 
   static #hasOnlyNumber(input) {
