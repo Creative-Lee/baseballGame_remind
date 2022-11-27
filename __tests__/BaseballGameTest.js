41093ad5aea5283ee101a1b6bef62486800fec82
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-11-27 12:49:58 +0900

    test(BaseballGameTest): setter 메서드 삭제로 인한 테스트 케이스 수정

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -5,9 +5,8 @@ const randomNumberMaker = require('../src/utils/randomNumberMaker.js');
 describe('BaseballGame 클래스 테스트', () => {
   describe('getCompareResult 메서드 테스트', () => {
     test('입력된 숫자와 컴퓨터 숫자를 비교한 결과를 객체로 return한다.', () => {
-      const computer = new Computer();
       const randomNumbers = [1, 2, 3];
-      computer.setRandomNumbers(randomNumbers);
+      const computer = new Computer(randomNumbers);
       const baseballGame = new BaseballGame(computer);
 
       const result = baseballGame.getCompareResult('132');
